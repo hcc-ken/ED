9233652f80c27195fe9787cd695e2aa1dd958c99
hcc <109288656+hcc-ken@users.noreply.github.com>
2026-02-19 22:54:12 +0100

    Add files via upload

---
+++ 2 Trimestre/simulacroExamen2/simulacroExamen2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simulacroExamen2
+{
+    public partial class Form1 : Form
+    {
+        double [] articulos = new double[5];
+        int contador = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (contador < articulos.Length)
+            {
+                articulos[contador] = Convert.ToDouble(textBox1.Text);
+                contador++;
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Ya se han ingresado los 5 artículos");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(articulos);
+            form2.ShowDialog();
+        }
+    }
+}
---
+++ 2 Trimestre/simulacroExamen2/simulacroExamen2/Form2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simulacroExamen2
+{
+    public partial class Form2 : Form
+    {
+        double[] lista = new double[5];
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public Form2(double[] articulos)
+        {
+            InitializeComponent();
+            lista = articulos;
+            MostrarInformacion();
+
+        }
+
+        private void MostrarInformacion()
+        {
+            string mensaje = "Los artículos son: \n";
+            double suma = 0;
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                mensaje = mensaje + "Producto " + (i + 1) + ": " + lista[i] + "\n";
+                suma += lista[i];
+            }
+
+            label1.Text = mensaje;
+            label2.Text = "El total a pagar es: " + suma;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
